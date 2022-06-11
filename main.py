919946ad0fe794b43f4c0209549f61e690d89a97
wszy5 <wikoszyman@wp.pl>
2022-06-12 00:31:46 +0200

    random word getter

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+import json
+import random
+
+with open("data.json",mode="r") as file:
+    x = json.load(file)
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"Data": "Test"}
+@app.get("random")
+def random_word():
+    return(random.choice(x))
+
+# with open("words.txt", mode="r") as file:
+#     content = [j.split(';') for j in [i.strip() for i in file.readlines()]]
+#     json = [{"word":i[0].strip(),"translation":i[1].strip(), "difficulty": 1} for i in content]
+
+
