e63a8884236bcc98a9def0dbc0ad78af7b579d98
wszy5 <wikoszyman@wp.pl>
2022-06-27 18:30:49 +0200

    small changes

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from fastapi import FastAPI
 import json
 import random
 
-with open("data.json",mode="r") as file:
-    x = json.load(file)
-print(x)
+
 app = FastAPI()
-random_quota = random.choice(x)
-@app.get("random")
+
+with open("data.json",mode="r",encoding="utf-8") as file:
+        x = json.load(file)
+
+@app.get("/random")
 def random_word():
-    return(x[0])
+    random_quota = random.choice(x)
+    return random_quota
 
 # with open("words.txt", mode="r") as file:
 #     content = [j.split(';') for j in [i.strip() for i in file.readlines()]]
 #     json = [{"word":i[0].strip(),"translation":i[1].strip(), "difficulty": 1} for i in content]
-
-# print(random.choice(x))
